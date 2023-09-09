9b25e706dd8ed683e47c3d6afd461bb18e5a8962
Banwind <a84012807@gmail.com>
2023-09-10 01:49:56 +0800

    add trade panel

--- src/pages/Targets/targetDetail.vue
+++ src/pages/Targets/targetDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="search">
+    <div class="search-container">
       <form class="form-inline ml-3">
         <div class="input-group input-group-sm">
           <input
@@ -20,9 +20,84 @@
         >Enter 'AAPL,' 'TSLA,' or 'BTCUSD' to search for the target.</span
       >
     </div>
-    <div class="chart" v-if="target">
-      <!-- TradingView 图表小部件容器 -->
-      <trading-target :target="target" wIndex="1"></trading-target>
+    <div class="chart-trade">
+      <div class="chart" v-if="target">
+        <!-- TradingView 图表小部件容器 -->
+        <trading-target :target="target" wIndex="1"></trading-target>
+      </div>
+      <div class="trade" v-if="target">
+        <div class="row justify-content-center">
+          <div class="content">
+            <form @submit.prevent="submitForm">
+              <div class="form-group">
+                <label for="buyType">Order Type</label>
+                <select class="form-control" id="buyType">
+                  <option>Limit</option>
+                  <option>Market</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="balance">Available Balance</label>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">USDT</span>
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="balance"
+                    value="10000"
+                    disabled
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="price">Price</label>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="price"
+                    min="0.01"
+                    step="0.01"
+                    value="25907.40"
+                  />
+                  <div class="input-group-append">
+                    <span class="input-group-text">USDT</span>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <input
+                  type="range"
+                  class="custom-range"
+                  id="slider"
+                  min="0"
+                  max="100"
+                  step="1"
+                />
+                <span id="sliderValue">0</span>
+              </div>
+              <div class="form-group">
+                <label for="quantity">Quantity</label>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="quantity"
+                    min="0.00001000"
+                    step="0.00001000"
+                  />
+                  <div class="input-group-append">
+                    <span class="input-group-text">BTC</span>
+                  </div>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Buy BTC</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +109,7 @@ import tradingTarget from "../../components/charts/tradingTarget.vue";
 const searchQuery = ref("");
 const target = ref("");
 
-const searchStock = (keyword) => {
+function searchStock(keyword) {
   target.value = "";
   console.log(target.value, keyword);
   const trimmedQuery = keyword.trim().toUpperCase();
@@ -44,7 +119,11 @@ const searchStock = (keyword) => {
     target.value = "";
     searchQuery.value = "";
   }
-};
+}
+
+function submitForm() {
+  console.log("submit");
+}
 
 watch(searchQuery, (newTarget) => {
   // 在 target 变化时，判断是否为空，然后清空 searchQuery
@@ -55,22 +134,79 @@ watch(searchQuery, (newTarget) => {
 </script>
 
 <style scoped>
+
+.search-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 居中对齐 */
+}
 .chart {
   /* 讓圖表大小能自動縮放 */
-  width: 80%;
+  width: 75%;
   height: 600px;
 }
 
 .desc {
   font-size: 0.8rem;
   color: #6c757d;
-  margin-left: 10px;
 }
 
-.search {
+.chart-trade {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
+  justify-content: space-evenly;
+}
+
+.trade {
+  width: 30%;
+  margin: auto auto;
+}
+
+.content {
+  width: 75%;
+  max-width: 500px;
+}
+
+/* 样式化输入范围滑块 */
+.custom-range {
+  width: 100%;
+  height: 1.4rem; /* 调整滑块高度 */
+  appearance: none; /* 隐藏默认样式 */
+  background-color: #fff; /* 背景颜色 */
+  border: none;
+  border-radius: 0.5rem; /* 圆角边框 */
+  outline: none;
+}
+
+/* 定义滑块轨道样式 */
+.custom-range::-webkit-slider-runnable-track {
+  height: 0.5rem; /* 轨道高度 */
+  background-color: #fff; /* 滑块轨道颜色 */
+  border-radius: 0.5rem; /* 圆角边框 */
+}
+
+/* 定义滑块样式 */
+.custom-range::-webkit-slider-thumb {
+  width: 1.6rem; /* 滑块宽度 */
+  height: 1.6rem; /* 滑块高度 */
+  background-color: #007bff; /* 滑块颜色 */
+  border: 2px solid #fff; /* 滑块边框 */
+  border-radius: 50%; /* 圆形滑块 */
+  appearance: none; /* 隐藏默认样式 */
+  cursor: pointer;
+  margin-top: -0.6rem; /* 调整滑块垂直居中 */
+}
+
+/* 定义滑块悬停样式 */
+.custom-range::-webkit-slider-thumb:hover {
+  background-color: #0056b3; /* 滑块悬停颜色 */
+}
+
+/* 样式化滑块当前值显示 */
+#sliderValue {
+  display: inline-block;
+  margin-left: 0.5rem; /* 调整当前值位置 */
+  font-size: 1rem; /* 字体大小 */
+  color: #007bff; /* 文字颜色 */
+  font-weight: bold;
 }
 </style>
