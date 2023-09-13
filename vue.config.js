62f26027b4e0e09020019814b98c13eaf134fdc4
Banwind <a84012807@gmail.com>
2023-09-14 02:14:07 +0800

    modify URL

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/exchange_frontend/'
-  //   : '/',
-  publicPath: '/',
-})
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/exchange_frontend/" : "/",
+});
