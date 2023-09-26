5dac88c999e3ab86b25187c5db241c427dc49e60
ben0128 <a84012807@gmail.com>
2023-09-26 18:37:16 +0800

    modify fb login

--- src/pages/Auth/UserAuth.vue
+++ src/pages/Auth/UserAuth.vue
@@ -257,7 +257,7 @@ onMounted(() => {
     if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    js.src = "https://connect.facebook.net/en_US/all.js";
     fjs.parentNode.insertBefore(js, fjs);
   })(document, "script", "facebook-jssdk");
 });
