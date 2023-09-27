dd7c5c044bddb54cf91a539732b939ed2dfd58ad
ben0128 <a84012807@gmail.com>
2023-09-27 20:57:54 +0800

    add favortarget

--- src/components/charts/favoriteTarget.vue
+++ src/components/charts/favoriteTarget.vue
@@ -1,45 +1,51 @@
 <template>
   <div class="tradingview-widget-container">
-    <div class="tradingview-widget-container__widget" :id="props.windex"></div>
+    <div v-bind:id="wIndexId" class="widget-index"></div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, defineProps } from "vue";
+import { onMounted, defineProps, computed } from "vue";
 
-const props = defineProps(["windex", "target"]);
+const props = defineProps(["windex", "targetName"]);
 
-onMounted(() => {
-  loadTradingViewWidget();
+const wIndexId = computed(() => {
+  return `widget-index-${props.windex}`;
 });
-function loadTradingViewWidget() {
+
+function loadTradingViewWidget(idStr) {
+  if (idStr == "") return;
   const script = document.createElement("script");
   script.type = "text/javascript";
   script.src =
-    "https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js";
+    "https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js";
   script.async = true;
 
-  // 定义 TradingView 小部件配置
-  const widgetConfig = {
-    symbol: "NASDAQ:TSLA",
-    width: "100%",
-    colorTheme: "dark",
-    isTransparent: false,
-    locale: "en",
-  };
-
+  // 定義 TradingView Widget 的設定
   script.innerHTML = `{
-        "symbol": "${widgetConfig.symbol}",
-        "width": "${widgetConfig.width}",
-        "colorTheme": "${widgetConfig.colorTheme}",
-        "isTransparent": ${widgetConfig.isTransparent},
-        "locale": "${widgetConfig.locale}"
+        "symbol": "${props.targetName}",
+        "width": "100%",
+        "colorTheme": "dark",
+        "isTransparent": false,
+        "locale": "en"
       }`;
 
   // 将脚本插入到 DOM 中
-  const container = document.querySelector(
-    "#windex .tradingview-widget-container__widget"
-  );
-  container.appendChild(script);
+  document.querySelector(`#${idStr}`).appendChild(script);
 }
+
+onMounted(() => {
+  loadTradingViewWidget(wIndexId.value);
+});
 </script>
+
+<style scoped>
+.widget-index {
+  /* 保持比例 */
+  width: 20rem;
+}
+.tradingview-widget-container {
+  margin: 1rem auto
+}
+
+</style>
--- src/pages/User/favoriteAssets.vue
+++ src/pages/User/favoriteAssets.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>
-    <p>3223</p>
+  <div class="target-list">
+    <favorite-target
+      v-for="like in likes"
+      :key="like.id"
+      :targetName="like.targetName"
+      :windex="like.id"
+    ></favorite-target>
   </div>
 </template>
 
-<script>
+<script setup>
+import favoriteTarget from "../../components/charts/favoriteTarget.vue";
 
+const likes = [
+  { id: 1, targetName: "AAPL" },
+  { id: 2, targetName: "GOOG" },
+  { id: 3, targetName: "AMZN" },
+  { id: 4, targetName: "TSLA" },
+  { id: 5, targetName: "MSFT" },
+  { id: 6, targetName: "FB" }
+];
 </script>
+
+<style scoped>
+.target-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+favorite-target {
+  margin-bottom: 1rem;
+}
+</style>
